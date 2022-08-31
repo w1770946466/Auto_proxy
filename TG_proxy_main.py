7da47388de00f57e3131dd745591aa9b119fddb7
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-08-31 16:25:13 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -157,7 +157,7 @@ def get_yaml():
 
 if __name__ == '__main__':
     urls = ["https://t.me/s/airproxies", "https://t.me/s/baipiao01",
-            "https://t.me/s/Jsnzk", "https://t.me/s/masco899", "https://t.me/s/fffffx2"]
+            "https://t.me/s/jokersfantastichome", "https://t.me/s/masco899", "https://t.me/s/fffffx2"]
     for url in urls:
         resp = get_content(get_channel_http(url))
         print(url, "获取完毕！！")
