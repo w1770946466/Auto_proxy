186aad9974fc44ea37b96180adbae1cacf9e9868
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-11-24 22:09:28 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -58,7 +58,9 @@ def jiemi_base64(data):  # 解密base64
 
 #判读是否为订阅链接
 def get_content(url):
+    print(url)
     url_lst = get_channel_http(url)
+    print(url_lst)
     #对链接进行格式化
     for i in url_lst:
         result = i.replace("\\", "").replace('"', "")
