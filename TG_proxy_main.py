02fd2d7be3367999f7ed3203da013e885815b7ba
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-12-07 01:52:42 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -239,7 +239,8 @@ def get_sub_url():
         'https://console.ly520.me',
         'https://www.jedi.ink',
         'https://www.yifei999.com',
-        'https://www.778778.ga'
+        'https://www.778778.ga',
+        'https://www.9666199.xyz'
     )
     times = 2
     for current_url in home_urls:
