f91b395321ee607396ee8b97587bfdc2aee45275
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-12-06 22:49:18 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -229,11 +229,6 @@ def get_yaml():
 def get_sub_url():
     V2B_REG_REL_URL = '/api/v1/passport/auth/register'
     # V2B_SUB_REL_URL = '/api/v1/user/getSubscribe'
-    header = {
-        'Referer': 'https://user.bafang.vip/',
-        'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Mobile/15E148 Safari/604.1',
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
     home_urls = (
         'https://user.bafang.vip',
         #'https://cloud.hhygj.xyz',
@@ -249,8 +244,13 @@ def get_sub_url():
     for current_url in home_urls:
         i = 0
         while i < times:
+            header = {
+                'Referer': current_url,
+                'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Mobile/15E148 Safari/604.1',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
             form_data = {
-                'email': '1770987654@gmail.com',
+                'email': ''.join(random.choice(string.ascii_letters+string.digits) for _ in range(12))+'@gmail.com',
                 'password': 'autosub_v2b',
                 'invite_code': '',
                 'email_code': ''
@@ -258,11 +258,10 @@ def get_sub_url():
             print(form_data)
             try:
                 response = requests.post(current_url+V2B_REG_REL_URL, data=form_data,headers=header)
-                print(response.text)
                 try:
-                    print(response.json()["data"]["token"])
                     subscription_url = f'{current_url}/api/v1/client/subscribe?token={response.json()["data"]["token"]}'
                     e_sub.append(subscription_url)
+                    print(subscription_url)
                 except:
                     print("获取订阅失败")
             except:
