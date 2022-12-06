015dc22b862c13bb1a7e80598a9df3e7af0ca93d
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-12-07 01:46:11 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -239,7 +239,7 @@ def get_sub_url():
         'https://console.ly520.me',
         'https://www.jedi.ink',
         'https://www.yifei999.com',
-        'https://www.71f.co'
+        'https://galaxysurf.xyz'
     )
     times = 2
     for current_url in home_urls:
