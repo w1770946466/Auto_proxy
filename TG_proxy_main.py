1e19c4c24dd764e966eb502f88029bb58982a54e
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-12-07 15:29:13 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -203,9 +203,9 @@ def write_document():
             if lines[index] == '>试用订阅：\n': # 目标行内容
                 lines.pop(index)
                 lines.pop(index+1)
-                lines.insert(index+1, f'\n\n\n\n\n\n\n')
+                lines.insert(index+1, f'\n\n')
             if lines[index] == '`https://raw.githubusercontent.com/w1770946466/Auto_proxy/main/Long_term_subscription3.yaml`\n': # 目标行内容
-                n = 1
+                n = 4
                 for TrySub in try_sub:
                     lines.insert(index+n-1, f'\n>\n')
                     lines.insert(index+n, f'>试用订阅：\n`{TrySub}`')
