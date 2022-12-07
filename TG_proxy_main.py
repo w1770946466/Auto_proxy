4aee03c6b253883fbf5189f3418c38113786911c
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-12-07 14:31:18 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -206,9 +206,10 @@ def write_document():
             if lines[index] == '`https://raw.githubusercontent.com/w1770946466/Auto_proxy/main/Long_term_subscription3.yaml`\n': # 目标行内容
                 n = 1
                 for TrySub in try_sub:
+                    lines.insert(index+n-1, f'>\n')
                     lines.insert(index+n, f'>试用订阅：\n`{TrySub}`')
                     n += 3
-                
+                lines.insert(index+n, f'\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
         with open("README.md", 'w', encoding='utf-8') as f:
             data = ''.join(lines)
             f.write(data)
