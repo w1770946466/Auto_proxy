3712fada7110d02dd8040aa7a531205392ed91a3
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-12-08 11:44:15 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -203,10 +203,10 @@ def write_document():
                     lines.insert(index+1, f'`合并节点总数: {length-step*7}`\n')
                 
                 if lines[index] == '>试用订阅：\n': # 目标行内容
-                    lines.pop(index-1)
+                    #lines.pop(index-1)
                     lines.pop(index)
-                    lines.pop(index+1)
-                    #lines.pop(index+2)
+                    #lines.pop(index+1)
+                    lines.pop(index+2)
                     #lines.insert(index+1, f'\n')
                     print(index,"删除试用成功")
                 """
