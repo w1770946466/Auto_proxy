2ba3bd98d89e879ac347fa97fd7dbb8c2ab68e1a
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-12-08 22:29:39 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -203,9 +203,9 @@ def write_document():
                 if lines[index] == '`https://raw.githubusercontent.com/w1770946466/Auto_proxy/main/Long_term_subscription8`\n': # 目标行内容
                     lines.pop(index+1)
                     lines.insert(index+1, f'`合并节点总数: {length-step*7}`\n')
-                if lines[index] == '### 试用高速订阅数量: `12`\n': # 目标行内容
-                    lines.pop(index+1)
-                    lines.insert(index+1, f'更新时间：`{TimeDate}`\n')
+                if lines[index] == '</details>\n': # 目标行内容
+                    lines.pop(index+3)
+                    lines.insert(index+3, f'更新时间：`{TimeDate}`\n')
                 if lines[index] == '>试用订阅：\n': # 目标行内容
                     lines.pop(index)
                     lines.pop(index)
