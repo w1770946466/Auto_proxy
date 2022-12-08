109c363dd634d4e709242e4d6dc0e03456cedc4c
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-12-08 14:39:58 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -204,7 +204,7 @@ def write_document():
                 
                 if lines[index] == '>试用订阅：\n': # 目标行内容
                     #lines.pop(index-1)
-                    #lines.pop(index)
+                    lines.pop(index)
                     lines.pop(index+1)
                     #lines.pop(index+2)
                     #lines.insert(index+1, f'\n')
