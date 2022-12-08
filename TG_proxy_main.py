a3f5f90b3e8613aa5ccbc57c3331274b0396f8f0
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-12-08 10:24:21 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -209,17 +209,26 @@ def write_document():
                     #lines.pop(index+2)
                     #lines.insert(index+1, f'\n')
                     print("删除试用成功")
-                #"""
+                """
                 if lines[index] == '## ✨星标统计\n': # 目标行内容
                     n = 5
                     for TrySub in try_sub:
                         #lines.insert(index+n-1, f'\n>')
                         lines.insert(index-n, f'\n>试用订阅：\n`{TrySub}`\n')
                         n += 3
-                #"""
+                """
             except:
                 print("写入READ出错")
-        
+        for index in range(len(lines)):
+            try:
+                if lines[index] == '## ✨星标统计\n': # 目标行内容
+                    n = 5
+                    for TrySub in try_sub:
+                        #lines.insert(index+n-1, f'\n>')
+                        lines.insert(index-n, f'\n>试用订阅：\n`{TrySub}`\n')
+                        n += 3
+            except:
+                print("写入试用出错")
         with open("README.md", 'w', encoding='utf-8') as f:
             data = ''.join(lines)
             f.write(data)
