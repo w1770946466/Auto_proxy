33b9d53665a448b5f2a18df76ac6df3b09044455
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-12-22 23:26:39 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -372,9 +372,8 @@ def get_kkzui():
     try:
         headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53"}
         res = requests.get("https://www.cfmem.com/search/label/free",headers=headers)
-        print(res.text)
-        #article_url = re.search(r'<a href="(.*?)" rel="bookmark" title="(.*?) 美国|台湾|日本|新加坡|英国 v2ray|clash订阅 手机电脑 科学上网|梯子|翻墙|代理|VPN">(.*?) 美国|台湾|日本|新加坡|英国 v2ray|clash订阅 手机电脑 科学上网|梯子|翻墙|代理|VPN</a>',res.text).groups()[0]
-        #print(article_url)
+        article_url = re.search(r"https?://www\.cfmem\.com/\d{4}/\d{2}/\S+v2rayclash-vpn.html",res.text).groups()[0]
+        print(article_url)
         '''
         res = requests.get(article_url,headers=headers)
         sub_url = re.search(r'<p><strong>这是v2订阅地址</strong>：(.*?)</p>',res.text).groups()[0]
