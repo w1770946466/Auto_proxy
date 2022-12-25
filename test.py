1ddbe75d90eff53a1951320061a3f8150b4f884d
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-12-25 19:18:34 +0800

    Update test.py

--- test.py
+++ test.py
@@ -336,7 +336,7 @@ if __name__ == '__main__':
     # 输出路径
     output_path = './output.yaml'
     # 规则策略
-    config_url = 'https://github.com/w1770946466/Auto_proxy/blob/main/config.yaml'
+    config_url = 'https://raw.githubusercontent.com/w1770946466/Auto_proxy/main/config.yaml'
     config_path = './config.yaml'
 
     if sub_url is None or sub_url == '':
