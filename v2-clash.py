ef5a3862f35a19db7a78155e4e1e017c040a9fd7
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-12-27 23:14:55 +0800

    Update v2-clash.py

--- v2-clash.py
+++ v2-clash.py
@@ -331,7 +331,7 @@ def get_default_config(url, path):
 # 将代理添加到配置文件
 def add_proxies_to_model(data, model):
     
-    print(data)
+    print(data['proxy_list'])
     #print(model)
     if data is None or model is None:
         raise ValueError('Invalid input: data and model cannot be None')
@@ -348,7 +348,8 @@ def add_proxies_to_model(data, model):
 
     try:
         data['proxy_names'] = list(set(data['proxy_names']))
-        #print(data['proxy_names'])
+        data['proxy_list'] = [d for d in data['proxy_list'] if 'name' in d]
+        print(data['proxy_list'])
         for group in model.get('proxy-groups'):
             if group.get('proxies') is None:
                 group['proxies'] = data.get('proxy_names')
