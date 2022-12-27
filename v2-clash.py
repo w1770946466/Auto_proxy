024ab209da5dbebe42ad4930767314bb88b00c58
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-12-28 02:38:23 +0800

    Update v2-clash.py

--- v2-clash.py
+++ v2-clash.py
@@ -382,7 +382,7 @@ def remove_duplicates(lst):
     for item in lst:
         if 'name' in item and item['name'] not in namesl:
             namesl.append(item['name'])
-            item['name'] = f'Auto{i}'
+            #item['name'] = f'Auto{i}'
             result.append(item)
             i += 1
     return result
