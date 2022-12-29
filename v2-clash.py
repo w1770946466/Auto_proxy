208775dd68d4f29884c1f9462a2f5a4b797a597b
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-12-29 22:56:40 +0800

    Update v2-clash.py

--- v2-clash.py
+++ v2-clash.py
@@ -471,7 +471,7 @@ def save_config(path, data):
 if __name__ == '__main__':
     # 订阅地址 多个地址用;隔开
     #sub_url = input('请输入订阅地址(多个地址用;隔开):')
-    sub_url = 'https://raw.githubusercontent.com/freefq/free/master/v2'
+    sub_url = 'https://raw.githubusercontent.com/w1770946466/Auto_proxy/main/Long_term_subscription7'
     # 输出路径
     output_path = './output.yaml'
     # 规则策略
