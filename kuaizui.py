fe71a7a5b3514b1a97c71916e8684691eb0a8c2f
北慕白 <63459931+w1770946466@users.noreply.github.com>
2023-04-15 22:13:09 +0800

    Create kuaizui.py

---
+++ kuaizui.py
@@ -0,0 +1,26 @@
+def get_kkzui():
+    # ========== 抓取 kkzui.com 的节点 ==========
+    try:
+        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53"}
+        res = requests.get("https://kkzui.com/jd?orderby=modified",headers=headers)
+        article_url = re.search(r'<h2 class="item-heading"><a href="(https://kkzui.com/(.*?)\.html)">20(.*?)号(.*?)个高速免费节点(.*?)免费代理</a></h2>',res.text).groups()[0]
+        #print(article_url)
+        res = requests.get(article_url,headers=headers)
+        sub_url = re.search(r'<p><strong>这是v2订阅地址</strong>：(.*?)</p>',res.text).groups()[0]
+        e_sub.append(sub_url)
+        print("获取kkzui.com完成！")
+    except:
+        print("获取kkzui.com失败！")
+    try:
+        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53"}
+        res = requests.get("https://www.cfmem.com/search/label/free",headers=headers)
+        article_url = re.search(r"https?://www\.cfmem\.com/\d{4}/\d{2}/\S+v2rayclash-vpn.html",res.text).group()
+        print(article_url)
+        res = requests.get(article_url,headers=headers)
+        sub_url = re.search(r'>v2ray订阅链接&#65306;(.*?)</span>',res.text).groups()[0]
+        print(sub_url)
+        e_sub.append(sub_url)
+    except Exception as e:
+        print(e)
+        
+get_kkzui()
