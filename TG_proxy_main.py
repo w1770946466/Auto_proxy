df553ba91e378ef6715eb34e03841980bc11f046
北慕白 <63459931+w1770946466@users.noreply.github.com>
2023-04-20 14:30:44 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -15,14 +15,21 @@ home_urls = (
     'https://dash.vmssr.co',
     'https://www.t3acc.top'
     'https://airport.xiaoheizis.xyz',
-    'https://www.ckcloud.xyz',
-    'https://fastestcloud.xyz',
     'https://lemontea.shop',
-    'https://user.bafang.vip',
-    'https://www.dgycom.com',
     'https://kuaimiao.cloud',
     'https://xunfan.co',
     'https://onlines7.cloud',
+    'https://seeworld.pro',          #5T   永久
+    'https://xn--4gqu8thxjfje.com',  #100G 永久
+    'https://www.dgycom.com',        #10G  2天
+    'https://fastestcloud.xyz',      #2G   1天
+    'https://satou.moe',             #2G   1天
+    'https://dash.vmssr.co',         #1G   1天
+    'https://www.ckcloud.xyz',       #1G   1天
+    'https://user.bafang.vip',       #1G   1天
+    'https://fly10086.top',          #1G   1天
+    'https://party668.com',          #1G   1天
+    'https://www.t3acc.top',         #1G   1天
 )
 #文件路径
 update_path = "./sub/"
@@ -313,7 +320,6 @@ def get_yaml():
 #获取机场试用订阅
 def get_sub_url():
     V2B_REG_REL_URL = '/api/v1/passport/auth/register'
-    # V2B_SUB_REL_URL = '/api/v1/user/getSubscribe'
     times = 1
     for current_url in home_urls:
         i = 0
@@ -329,57 +335,49 @@ def get_sub_url():
                 'invite_code': '',
                 'email_code': ''
             }
-            #try:
-                #print(current_url)
-                #response = requests.post(current_url+V2B_REG_REL_URL, data=form_data,headers=header)
-            #except:
-                #print("获取订阅失败")
-            '''
-            if current_url == 'https://meal.leftright.buzz':
+            if current_url == 'https://xn--4gqu8thxjfje.com' or current_url == 'https://seeworld.pro':
                 try:
-                    #print(current_url)
-                    fan_res = requests.post('https://meal.leftright.buzz/api/v1/passport/auth/login', data=form_data,headers=header)
-                    print(fan_res.text)
-                    sleep(3)
+                    fan_res = requests.post(
+                        f'{current_url}/api/v1/passport/auth/register', data=form_data, headers=header)
                     auth_data = fan_res.json()["data"]["auth_data"]
-                    print(auth_data)
+                    #print(auth_data)
                     fan_header = {
-                        'Host': 'meal.leftright.buzz',
-                        'Origin': 'https://meal.leftright.buzz',
+                        'Origin': current_url,
                         'Authorization': ''.join(auth_data),
                         'Content-Type': 'application/x-www-form-urlencoded',
                         'Connection': 'keep-alive',
                         'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Mobile/15E148 Safari/604.1',
-                        'Referer': 'https://meal.leftright.buzz/',
+                        'Referer': current_url,
                     }
                     fan_data = {
-                        'period': 'quarter_price',
+                        'period': 'onetime_price',
                         'plan_id': '1',
                     }
-                    fan_res_n = requests.post('https://meal.leftright.buzz/api/v1/user/order/save', headers=fan_header, data=fan_data)
-                    print(fan_res_n.json()["data"])
+                    fan_res_n = requests.post(
+                        f'{current_url}/api/v1/user/order/save', headers=fan_header, data=fan_data)
+                    #print(fan_res_n.json()["data"])
                     fan_data_n = {
                         'trade_no':fan_res_n.json()["data"],
-                        'method': '1',
+                        #'method': '1',
                     }
-                    fan_res_pay = requests.post('https://meal.leftright.buzz/api/v1/user/order/checkout', data=fan_data_n,headers=fan_header)
-                    print("获取饭小溪订阅成功。。。",fan_res_pay.text)
-                
+                    fan_res_pay = requests.post(
+                        f'{current_url}/api/v1/user/order/checkout', data=fan_data_n, headers=fan_header)
+                    subscription_url = f'{current_url}/api/v1/client/subscribe?token={fan_res.json()["data"]["token"]}'
+                    e_sub.append(subscription_url)
+                    print("add:"+subscription_url)
                 except Exception as result:
                     print(result)
                     break
-            '''
-            try:
-                response = requests.post(current_url+V2B_REG_REL_URL, data=form_data,headers=header)
-                subscription_url = f'{current_url}/api/v1/client/subscribe?token={response.json()["data"]["token"]}'
-                e_sub.append(subscription_url)
-                try_sub.append(subscription_url)
-                #print(subscription_url)
-                print("add:"+subscription_url)
-            except:
-                print("获取订阅失败")
+            else:
+                try:
+                    response = requests.post(
+                        current_url+V2B_REG_REL_URL, data=form_data, headers=header)
+                    subscription_url = f'{current_url}/api/v1/client/subscribe?token={response.json()["data"]["token"]}'
+                    e_sub.append(subscription_url)
+                    print("add:"+subscription_url)
+                except:
+                    print("获取订阅失败")
             i += 1
-            #print(f'Number succeeded: {i}\t{subscription_url}')
 
             
   
