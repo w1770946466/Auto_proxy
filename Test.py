bc83322921fc09da7dc0f608c3ddf3421cd31379
北慕白 <63459931+w1770946466@users.noreply.github.com>
2023-04-23 00:18:36 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,86 +1,17 @@
 import requests
-import random
-import string
-
-e_sub = []
 
 
+def get_yaml():
+    print("开始获取clsah订阅")
+    urls = ["https://api.dler.io//sub?target=clash&url=https://raw.githubusercontent.com/w1770946466/Auto_proxy/main/Long_term_subscription_try&insert=false&config=https://raw.githubusercontent.com/w1770946466/fetchProxy/main/config/provider/rxconfig.ini&emoji=true","https://api.dler.io//sub?target=clash&url=https://raw.githubusercontent.com/w1770946466/Auto_proxy/main/Long_term_subscription2&insert=false&config=https://raw.githubusercontent.com/w1770946466/fetchProxy/main/config/provider/rxconfig.ini&emoji=true"]
+    n = 1
+    for i in urls:
+        response = requests.get(i)
+        #print(response.text)
+        file_L = open("output.yaml", 'w', encoding='utf-8')
+        file_L.write(response.text)
+        file_L.close()
+        n += 1
+    print("clash订阅获取完成！")
 
-
-#获取机场试用订阅
-def get_sub_url():
-    V2B_REG_REL_URL = '/api/v1/passport/auth/register'
-    # V2B_SUB_REL_URL = '/api/v1/user/getSubscribe'
-    home_urls = (
-        'https://seeworld.pro',          #5T   永久
-        'https://xn--4gqu8thxjfje.com',  #100G 永久
-        'https://www.dgycom.com',        #10G  2天
-        'https://fastestcloud.xyz',      #2G   1天
-        'https://satou.moe',             #2G   1天
-        'https://dash.vmssr.co',         #1G   1天
-        'https://www.ckcloud.xyz',       #1G   1天
-        'https://user.bafang.vip',       #1G   1天
-        'https://fly10086.top',          #1G   1天
-        'https://party668.com',          #1G   1天
-        'https://www.t3acc.top',         #1G   1天
-    )
-    times = 1
-    for current_url in home_urls:
-        i = 0
-        while i < times:
-            header = {
-                'Referer': current_url,
-                'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Mobile/15E148 Safari/604.1',
-                'Content-Type': 'application/x-www-form-urlencoded',
-            }
-            form_data = {
-                'email': ''.join(random.choice(string.ascii_letters+string.digits) for _ in range(12))+'@gmail.com',
-                'password': 'autosub_v2b',
-                'invite_code': '',
-                'email_code': ''
-            }
-            if current_url == 'https://xn--4gqu8thxjfje.com' or current_url == 'https://seeworld.pro':
-                try:
-                    fan_res = requests.post(
-                        f'{current_url}/api/v1/passport/auth/register', data=form_data, headers=header)
-                    auth_data = fan_res.json()["data"]["auth_data"]
-                    #print(auth_data)
-                    fan_header = {
-                        'Origin': current_url,
-                        'Authorization': ''.join(auth_data),
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                        'Connection': 'keep-alive',
-                        'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Mobile/15E148 Safari/604.1',
-                        'Referer': current_url,
-                    }
-                    fan_data = {
-                        'period': 'onetime_price',
-                        'plan_id': '1',
-                    }
-                    fan_res_n = requests.post(
-                        f'{current_url}/api/v1/user/order/save', headers=fan_header, data=fan_data)
-                    #print(fan_res_n.json()["data"])
-                    fan_data_n = {
-                        'trade_no':fan_res_n.json()["data"],
-                        #'method': '1',
-                    }
-                    fan_res_pay = requests.post(
-                        f'{current_url}/api/v1/user/order/checkout', data=fan_data_n, headers=fan_header)
-                    subscription_url = f'{current_url}/api/v1/client/subscribe?token={fan_res.json()["data"]["token"]}'
-                    e_sub.append(subscription_url)
-                    print("add:"+subscription_url)
-                except Exception as result:
-                    print(result)
-                    break
-            else:
-                try:
-                    response = requests.post(
-                        current_url+V2B_REG_REL_URL, data=form_data, headers=header)
-                    subscription_url = f'{current_url}/api/v1/client/subscribe?token={response.json()["data"]["token"]}'
-                    e_sub.append(subscription_url)
-                    print("add:"+subscription_url)
-                except:
-                    print("获取订阅失败")
-            i += 1
-
-get_sub_url()
+get_yaml()
