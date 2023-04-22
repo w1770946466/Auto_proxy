f596bfd528e7c12626e07382b3df2caaa6f16161
北慕白 <63459931+w1770946466@users.noreply.github.com>
2023-04-22 11:48:35 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -375,6 +375,7 @@ def get_kkzui():
         #print(article_url)
         res = requests.get(article_url,headers=headers)
         sub_url = re.search(r'<p><strong>这是v2订阅地址</strong>：(.*?)</p>',res.text).groups()[0]
+        print(sub_url)
         e_sub.append(sub_url)
         print("获取kkzui.com完成！")
     except:
@@ -383,10 +384,11 @@ def get_kkzui():
         headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53"}
         res = requests.get("https://www.cfmem.com/search/label/free",headers=headers)
         article_url = re.search(r"https?://www\.cfmem\.com/\d{4}/\d{2}/\S+v2rayclash-vpn.html",res.text).group()
-        print(article_url)
+        #print(article_url)
         res = requests.get(article_url,headers=headers)
         sub_url = re.search(r'>v2ray订阅链接&#65306;(.*?)</span>',res.text).groups()[0]
         print(sub_url)
+        try_sub.append(sub_url)
         e_sub.append(sub_url)
     except Exception as e:
         print(e)
