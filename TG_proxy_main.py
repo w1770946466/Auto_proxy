067a481f944d8673d25ef3cc09280846a4058964
北慕白 <63459931+w1770946466@users.noreply.github.com>
2024-01-03 15:16:29 +0800

    Update TG_proxy_main.py
    ADD v2rayshare.com

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -381,12 +381,31 @@ def get_kkzui():
         e_sub.append(sub_url)
     except Exception as e:
         print(e)
+     # ========== 抓取 cfmem.com 的节点 ==========
+    try:
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53"}
+        res = requests.get(
+            "https://v2rayshare.com/", headers=headers)
+        #print(res.text)
+        article_url = re.search(
+            r'https://v2rayshare.com/p/\d+\.html', res.text).group()
+        #print(article_url)
+        res = requests.get(article_url, headers=headers)
+        sub_url = re.search(
+            r'https://v2rayshare.com/wp-content/uploads/\d+/\d+/\d+.txt', res.text).groups()[0]
+        print(sub_url)
+        try_sub.append(sub_url)
+        e_sub.append(sub_url)
+    except Exception as e:
+        print(e)
+
         
     
 if __name__ == '__main__':
     print("========== 开始获取机场订阅链接 ==========")
     get_sub_url()
-    print("========== 开始获取kkzui.com订阅链接 ==========")
+    print("========== 开始获取网站订阅链接 ==========")
     get_kkzui()
     print("========== 开始获取频道订阅链接 ==========")
     for url in urls:
